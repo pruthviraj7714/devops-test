728ee957d559ccac65bcf93728fa46eea5a7c64a
pruthviraj7714 <marganepruthviraj@gmail.com>
2025-06-27 06:36:33 +0000

    ci/cd test

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -16,8 +16,8 @@ export default function Home() {
   return (
     <div className="h-screen flex flex-col items-center">
         <p className="text-center text-5xl text-pink-500 font-extrabold">This is test devops deployment project</p>
-        <div className="flex justify-evenly items-center">
-          <button onClick={handleAddUser}>
+        <div className="flex justify-evenly gap-5 items-center">
+          <button className="bg-red-400 text-white text-xl border px-6 py-1" onClick={handleAddUser}>
             Add users
           </button>
           <Link href="/users">
